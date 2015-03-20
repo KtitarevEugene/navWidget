876123ea397c00150d7e06d36fb972f19035bb0a
Ktitarev Eugene <ktitarev.evgeniy@yandex.ru>
2015-03-21 01:59:03 +0300

    front leaving-through complete

--- app/widgets/PagedWidget/controllers/widget.js
+++ app/widgets/PagedWidget/controllers/widget.js
@@ -1,11 +1,46 @@
-var backButtonHandler = null;
+var backButtonHandler = function(e) {
+	var hidingView = stack[stack.length - 1];
+	var openingView = stack[stack.length - 2];
+	
+	var openingViewMatrix = Ti.UI.create2DMatrix();
+	openingViewMatrix = openingViewMatrix.scale(1, 1);
+	
+	var hidingViewMatrix = Ti.UI.create2DMatrix();
+	hidingViewMatrix = hidingViewMatrix.translate(400, 0);
+	
+	var openAnimation = Ti.UI.createAnimation({
+		transform: openingViewMatrix,
+		duration: 300
+	});
+	
+	openAnimation.addEventListener("complete", function() {
+		titles.pop();
+		$.currentWindowTitle.text = titles[titles.length - 1];
+	});
+	
+	var hideAnimation = Ti.UI.createAnimation({
+		transform: hidingViewMatrix,
+		duration: 300
+	});
+	
+	hideAnimation.addEventListener("complete", function() {
+		stack.pop();
+		$.content.remove(hidingView);
+		if(stack.length < 2)
+			$.icon.visible = false;
+	});
+	
+	openingView.show();
+	openingView.animate(openAnimation);
+	hidingView.animate(hideAnimation);
+};
 
 var stack = [];
-
+var titles = [];
 
 function backButtonClick (e) {
 	if(typeof(backButtonHandler) === "function")
-		backButtonHandler(e);	
+		backButtonHandler(e);
 }
 
 exports.onBackButtonClick = function(handler) {
@@ -27,6 +62,9 @@ exports.openPage = function (title, view) {
 	});
 	openAnimation.addEventListener("complete", function() {
 		stack.push(view);
+		titles.push(title);
+		view.setLeft(leftPos);
+		view.setRight(rightPos);
 		$.currentWindowTitle.text = title;
 		$.icon.visible = true;
 	});
@@ -42,6 +80,10 @@ exports.openPage = function (title, view) {
 	});
 	
 	$.content.add(view);
+	
+	var leftPos = view.getLeft();
+	var rightPos = view.getRight();
+	
 	view.setLeft(200);
 	view.setRight(-200);
 	hidingView.animate(hideAnimation);
@@ -53,5 +95,6 @@ exports.setFirstPage = function(title, view) {
 	$.icon.visible = false;
 	$.currentWindowTitle.text = title;
 	stack.push(view);
+	titles.push(title);
 	$.content.add(view);
 };
