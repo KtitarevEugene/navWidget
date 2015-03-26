7e855cc48686e7be7606f662803133bb945ebfca
unknown <ektitarev@WS12-PC.milane>
2015-03-26 13:21:37 +0300

    handlers implemented

--- app/widgets/PagedWidget/controllers/widget.js
+++ app/widgets/PagedWidget/controllers/widget.js
@@ -1,14 +1,53 @@
+var platformWidth = (OS_ANDROID) ? Ti.Platform.DisplayCaps().displayCaps.platformWidth : Ti.Platform.displayCaps.platformWidth;
+
+exports.addEventListener = $.widget.addEventListener;
+
+exports.on = function(type, handler) {
+	return $.widget.addEventListener(type, handler);
+};
+
+exports.off = function(type, handler) {
+	return $.widget.removeEventListener(type, handler);
+};
+ 
+exports.bind = function(type, handler) {
+	return $.widget.addEventListener(type, handler);
+};
+
+exports.unbind = function(type, handler) {
+	return $.widget.removeEventListener(type, handler);
+}; 
+
+exports.addEventListener = function(type, handler) {
+	return $.widget.addEventListener(type, handler);
+};
+exports.removeEventListener = function(type, handler) {
+	return $.widget.removeEventListener(type, handler);
+};
+ 
+exports.trigger = function(type, event) {
+	return $.widget.trigger(type, event);
+};
+exports.fireEvent = function(type, event) {
+	return $.widget.fireEvent(type, event);
+};
+
+exports._hasListenersForEventType = function(name, flag) {
+    return $.widget._hasListenersForEventType(name, flag);
+};
+
 var backButtonHandler = function(e) {
-	if(stack[stack.length - 1] && stack[stack.length - 2])
-	{
+	if(stack[stack.length - 1] && stack[stack.length - 2]) {
 		var hidingView = stack[stack.length - 1].component;
 		var openingView = stack[stack.length - 2].component;
 	
 		var openingViewMatrix = Ti.UI.create2DMatrix();
 		openingViewMatrix = openingViewMatrix.scale(1, 1);
 		
+		
+		
 		var hidingViewMatrix = Ti.UI.create2DMatrix();
-		hidingViewMatrix = hidingViewMatrix.translate(Ti.Platform.DisplayCaps().displayCaps.platformWidth * 0.8, 0);
+		hidingViewMatrix = hidingViewMatrix.translate(platformWidth * 0.8, 0);
 		
 		var openAnimation = Ti.UI.createAnimation({
 			transform: openingViewMatrix,
@@ -34,11 +73,29 @@ var backButtonHandler = function(e) {
 			if(stack.length < 2)
 				$.icon.visible = false;
 			isInAction = false;
+			
+			$.widget.fireEvent('back', {
+				type: "click",
+				source: $.widget,
+				title: stack[stack.length - 1].windowTitle,
+				pageNumber: stack.length,
+				pageView: openingView,
+				actionBarElements: stack[stack.length - 1].actionBarElements
+			});
 		});
 		
 		openingView.show();
 		openingView.animate(openAnimation);
 		hidingView.animate(hideAnimation);
+	} else {
+		$.widget.fireEvent('back', {
+			type: "click",
+			source: $.widget,
+			title: null,
+			pageNumber: 0,
+			pageView: null,
+			actionBarElements: null
+		});
 	}
 };
 
@@ -52,6 +109,19 @@ function backButtonClick (e) {
 
 exports.onBackButtonClick = function(handler) {
 	backButtonHandler = handler;
+};
+
+exports.back = function() {
+	if(typeof(backButtonHandler) === "function")
+		backButtonHandler();	
+};
+
+exports.setTitleBarBackgroundColor = function(color) {
+	$.header.backgroundColor = color;
+};
+
+exports.setTitleBarGradient = function(gradient) {
+	$.header.backgroundGradient = gradient;
 };
 
 exports.openPage = function (title, view) {
@@ -70,7 +140,7 @@ exports.openPage = function (title, view) {
 			}
 	
 		var openingViewMatrix = Ti.UI.create2DMatrix();
-		openingViewMatrix = openingViewMatrix.translate(-Ti.Platform.DisplayCaps().displayCaps.platformWidth * 0.8, 0);
+		openingViewMatrix = openingViewMatrix.translate(-platformWidth * 0.8, 0);
 		
 		var hidingViewMatrix = Ti.UI.create2DMatrix();
 		hidingViewMatrix = hidingViewMatrix.scale(0.5, 0.5);
@@ -110,8 +180,8 @@ exports.openPage = function (title, view) {
 		var leftPos = view.getLeft();
 		var rightPos = view.getRight();
 		
-		view.setLeft(Ti.Platform.DisplayCaps().displayCaps.platformWidth * 0.4);
-		view.setRight(-Ti.Platform.DisplayCaps().displayCaps.platformWidth * 0.4);
+		view.setLeft(platformWidth * 0.4);
+		view.setRight(-platformWidth * 0.4);
 		hidingView.animate(hideAnimation);
 		view.animate(openAnimation);
 	}
